ca12811716f564625ff677ba26993734bf8d4344
vadym.kosytskyi <kosvad02@gmail.com>
2022-10-26 23:22:01 +0300

    Add Abstract Circle, White and Black Circle

---
+++ src/main/java/com/groupproject/circle/AbstractCircle.java
@@ -0,0 +1,46 @@
+package com.groupproject.circle;
+
+import com.groupproject.Color;
+
+public abstract class AbstractCircle implements Circle {
+    protected double radius;
+    protected final Color color;
+
+
+    public AbstractCircle(Color color) {
+        radius = 1;
+        this.color = color;
+    }
+
+    public AbstractCircle(double radius, Color color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public String toString() {
+        return "Circle:" +
+                "\n\tradius= " + radius +
+                "\n\tcolor = " + color;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println(this);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+}
+
+
+
---
+++ src/main/java/com/groupproject/circle/BlackCircle.java
@@ -0,0 +1,15 @@
+package com.groupproject.circle;
+
+import com.groupproject.Color;
+
+public class BlackCircle extends AbstractCircle {
+
+    public BlackCircle(double radius) {
+        super(radius, Color.BLACK);
+
+    }
+    public BlackCircle(){
+        super(Color.BLACK);
+    }
+
+}
