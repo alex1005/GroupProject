e0e69a3adef1282cd1e49d3ef7dea6cd281352c7
Oleksiy <andriyshabo@gmail.com>
2022-10-27 17:21:00 +0300

    Remove redundant "" in println()

--- src/main/java/com/groupproject/Runner.java
+++ src/main/java/com/groupproject/Runner.java
@@ -5,35 +5,35 @@ public class Runner {
         System.out.println("Creating factories:");
         BaseFactory baseFactoryBlack = AbstractFactory.getFactory(Color.BLACK);
         BaseFactory baseFactoryWhite = AbstractFactory.getFactory(Color.WHITE);
-        System.out.println("");
-        System.out.println("");
+        System.out.println();
+        System.out.println();
 
         System.out.println("Creating default triangles:");
         baseFactoryBlack.createTriangle();
-        System.out.println("");
+        System.out.println();
         baseFactoryWhite.createTriangle();
-        System.out.println("");
-        System.out.println("");
+        System.out.println();
+        System.out.println();
 
         System.out.println("Creating triangles with parameters:");
         baseFactoryBlack.createTriangle(1.2, 1.2, 1.2);
-        System.out.println("");
+        System.out.println();
         baseFactoryWhite.createTriangle(1.3, 1.3, 1.3);
-        System.out.println("");
-        System.out.println("");
+        System.out.println();
+        System.out.println();
 
         System.out.println("Creating circles:");
         baseFactoryBlack.createCircle();
-        System.out.println("");
+        System.out.println();
         baseFactoryWhite.createCircle();
-        System.out.println("");
-        System.out.println("");
+        System.out.println();
+        System.out.println();
 
         System.out.println("Creating circles with parameters:");
         baseFactoryBlack.createCircle(1.2);
-        System.out.println("");
+        System.out.println();
         baseFactoryWhite.createCircle(1.3);
-        System.out.println("");
-        System.out.println("");
+        System.out.println();
+        System.out.println();
     }
 }
